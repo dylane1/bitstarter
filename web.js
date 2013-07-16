8591f205ca9a899e247bf84211d66860d759a10b
Dylan <dylan@dylanedwards.net>
2013-07-15 18:48:09 -0700

    Removed a bunch of the Initializr stuff as I don't need it for this project. Also posting a hosed version of index.html

--- web.js
+++ web.js
@@ -11,7 +11,12 @@ app.get('/', function(request, response) {
   response.send(buffer.toString());
 });
 
+app.on('error', function(err) {
+	console.error("error: ", err);
+});
+
 var port = process.env.PORT || 8080;
+
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
